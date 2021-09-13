8821468405542da804e586d6e275aa7e9f6dae6e
DRAHUL <86136418+Rahulgithub07@users.noreply.github.com>
2021-09-13 17:08:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import ReactDOM, { render } from "react-dom"
-import axios from 'axios';
+import ReactDOM from "react-dom"
+// import axios from 'axios';
 
 
 
 import PostApp from "./PostApp"
-import AppFunc from "./AppFunc"
+// import AppFunc from "./AppFunc"
 
 
 const ele = document.getElementById("root");
